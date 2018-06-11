bf56b08a8c8340be256b099b7f333e7c124fdb2e
feyerina <31159141+feyerina@users.noreply.github.com>
2018-06-11 23:38:31 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,1 +1,11 @@
-
+var str = process.argv[2];
+var arr=[];
+var regExps={
+    vowels: /[AEIOU]/gi,
+    consonants: /[BCDFGHJKLMNPQRSTVWXYZ]/gi,
+    digits: /\d/g
+}
+for(key in regExps){
+   (str.match(regExps[key]))?arr.push(str.match(regExps[key]).join("")):"";
+}
+process.stdout.write(arr.join(" "));

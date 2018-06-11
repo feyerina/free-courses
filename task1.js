8294fd02083ee34528c32e5f2fa129a4eb22c80c
feyerina <31159141+feyerina@users.noreply.github.com>
2018-06-11 21:39:11 +0300

    Delete task1.js

--- task1.js
+++
@@ -1,3 +0,0 @@
-var removeChar = process.argv[2].replace(/[^A-Z0-9]/ig, "").toLowerCase();
-var checkPalindrome = removeChar.split('').reverse().join('');
-process.stdout.write(removeChar === checkPalindrome);

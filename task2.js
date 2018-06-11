2bcf831355d260e699ed20779cb9f3472721530f
feyerina <31159141+feyerina@users.noreply.github.com>
2018-06-11 23:21:27 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -3,4 +3,4 @@ var sq5 = Math.sqrt(5);
 var a = (1 + sq5) / 2;
 var b = (1 - sq5) / 2;
 var result = (Math.pow(a, n) - Math.pow(b, n)) / sq5;
-rocess.stdout.write(result);
+process.stdout.write(result);

4878e541754217e4e6b2d4389358067802b71913
feyerina <31159141+feyerina@users.noreply.github.com>
2018-06-11 23:29:03 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,3 +1,7 @@
 var removeChar = process.argv[2].replace(/[^A-Z0-9]/ig, "").toLowerCase();
 var checkPalindrome = removeChar.split('').reverse().join('');
-process.stdout.write(removeChar === checkPalindrome);
+if (removeChar === checkPalindrome) {
+  process.stdout.write("YES");
+} else {
+  process.stdout.write("NO");
+}

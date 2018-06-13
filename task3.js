f32443d5d6e1ab3b356c31be65f94ac69b1614b4
feyerina <31159141+feyerina@users.noreply.github.com>
2018-06-13 21:47:43 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,3 +1,4 @@
+/*
 var str = process.argv[2];
 var arr=[];
 var regExps={
@@ -9,3 +10,41 @@ for(key in regExps){
    (str.match(regExps[key]))?arr.push(str.match(regExps[key]).join("")):"";
 }
 process.stdout.write(arr.join(" "));
+*/
+
+var l = process.argv.length-2;
+var getStr = process.argv[2];
+var i = 2;
+while(l > 1){
+  i++;
+var str = process.argv[i];
+getStr = getStr + str;
+l--;
+}
+
+function strDel(n){
+  var s='';
+  var a=n.match(/[aeiouy]/ig);
+  var b=n.match(/[bcdfghjklmnpqrstvwxz]/ig);
+  var c=n.match(/[0123456789]/ig);
+  if(a!==null){
+  for (var i=0;i<a.length;i++)
+  s+=a[i];
+  s+=' ';
+  }
+
+  if(b!==null){
+  for(var i=0;i<b.length;i++)
+  s+=b[i];
+  }
+  s+=' ';
+  if(c!==null){
+  for(var i=0;i<c.length;i++)
+    s+=c[i];
+  }
+  
+    return s;
+}
+
+
+process.stdout.write(strDel(getStr));

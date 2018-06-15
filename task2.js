30406c510235d59605c16c0511ba3419dc018b10
feyerina <31159141+feyerina@users.noreply.github.com>
2018-06-15 22:59:51 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,12 +1,15 @@
 function fibonachi(n) {
-    var a = 1;
+    var a = 0;
     var b = 1;
     var flag = false;
     if (n < 0){
         n *= -1;
         flag = true;
+    }    
+    if (n == 0) {
+       return n;
     }
-    for (var i = 3; i <= n; i++) {
+    for (var i = 2; i <= n; i++) {
         var c = a + b;
         a = b;
         b = c;
